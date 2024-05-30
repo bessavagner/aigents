664ad5e411c8346bf2495dc3ed1d77b1a271263f
bessavagner <bessavagner@gmail.com>
2024-05-30 09:40:26 -0300

    fix: custom log formaters

--- src/aigents/settings.py
+++ src/aigents/settings.py
@@ -50,14 +50,14 @@ Authors: {author}
 CONFIG_LOG = {
     "version": 1,
     "formatters": {
-        "client": {"format": "%(levelname)s: %(message)s"},
-        "standard": {
+        "aigents_default": {"format": "%(levelname)s: %(message)s"},
+        "aigents_standard": {
             "format": (
                 "%(levelname)s (at %(pathname)s - %(funcName)s "
                 "in line %(lineno)d): %(message)s"
             )
         },
-        "debug": {
+        "aigents_debug": {
             "format": (
                 "%(asctime)s %(levelname)s (at %(funcName)s "
                 "in line %(lineno)d):"
@@ -69,32 +69,31 @@ CONFIG_LOG = {
         }
     },
     "handlers": {
-        "client": {
+        "aigents_client": {
             "class": "logging.StreamHandler",
-            "formatter": "client",
+            "formatter": "aigents_default",
             "level": "INFO"
         },
-        "standard": {
+        "aigents_standard": {
             "class": "logging.StreamHandler",
-            "formatter": "standard",
+            "formatter": "aigents_standard",
             "level": "DEBUG"
         },
-        "debug": {
+        "aigents_debug": {
             "class": "logging.StreamHandler",
-            "formatter": "debug",
+            "formatter": "aigents_debug",
             "level": "DEBUG"
         }
     },
-    "root": {"handlers": ["standard"], "level": "DEBUG"},
     "loggers": {
-        "client": {
-            "handlers": ["client"],
+        "aigents_client": {
+            "handlers": ["aigents_client"],
             "level": "DEBUG",
             "propagate": False,
             "disable_existing_loggers": False
         },
         "aigents": {
-            "handlers": ["standard"],
+            "handlers": ["aigents_standard"],
             "level": "DEBUG",
             "propagate": False,
             "disable_existing_loggers": False
